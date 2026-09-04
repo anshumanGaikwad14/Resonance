f2467f56ef619af30a793afa22885b1c1b709c91
anshumanGaikwad14 <gaanshuman14@gmail.com>
2026-09-04 12:20:10 +0530

    01 Project setup Authentication and database

--- src/hooks/use-mobile.ts
+++ src/hooks/use-mobile.ts
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/set-state-in-effect */
+
 import * as React from "react";
 
 const MOBILE_BREAKPOINT = 1024;
@@ -13,7 +15,9 @@ export function useIsMobile() {
       setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
     };
     mql.addEventListener("change", onChange);
+
     setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+
     return () => mql.removeEventListener("change", onChange);
   }, []);
 
